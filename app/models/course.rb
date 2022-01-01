1c459aad5e2da005a7de5d57a1a3e950236d34d8
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-01 18:40:36 -0500

    Add student_count instance method to course model.

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,8 @@ class Course < ApplicationRecord
   has_many :student_courses
   has_many :students, through: :student_courses
   validates :name, presence: true
+
+  def student_count
+    students.count
+  end
 end
